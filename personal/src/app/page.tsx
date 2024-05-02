a690f60b55308c1aaf86ad543b25e7062cc0f9dc
GavinGoGaming <gavingogaming@gmail.com>
2024-05-02 00:19:33 +0000

    Create isadev website

--- personal/src/app/page.tsx
+++ personal/src/app/page.tsx
@@ -1,94 +1,84 @@
+"use client";
+import { marked } from "marked";
 import Image from "next/image";
-import styles from "./page.module.css";
+import { useEffect, useState } from "react";
+
+declare const window: any;
 
 export default function Home() {
+  const [lines, updLines] = useState<string[]>(["Terminal v1.0.0 [by Gavin, in React]"]);
+  function setLines(newLines: string[]) {
+    const markedLines = newLines.map(line => marked(line));
+    updLines(markedLines as string[]);
+  }
+  function runCommand(value: string) {
+    // Create a new array from the current lines
+    let newLines = [...lines, `<span style="color:#0f0;">gavingogaming</span><span style="color:#f00;">@</span><span style="color:#0f0;">personal:</span><span style="color:#00f;">~</span><span style="color:#ff0;">$</span> ${value}`];
+  
+    const args = value.split(" "); 
+    switch (args[0]) {
+      case "help":
+        newLines = [
+          ...newLines,
+          "[ list of commands ]",
+          "{ help, clear, projects, socials }"
+        ];
+        break;
+      case "clear":
+        newLines = ["Terminal v1.0.0 [by Gavin, in React]"];
+        break;
+      case "projects":
+        newLines = [
+          ...newLines,
+          "[ list of projects ]",
+          "{ [klash](https://klash.dev), [counterfeit](https://counterfeitcontent.com), [empires](https://designingempires.com), [glacier](https://glacier.fly.dev), [klips](https://klips.gavingogaming.com) }"
+        ];
+        break;
+      case "socials":
+        newLines = [
+          ...newLines,
+          "[ all my socials ]",
+          "{ [twitter](https://x.com/gavingogaming), [instagram](https://instagram.com/gavingogaming), [github](https://github.com/gavingogaming), [youtube](https://youtube.com/gavingogaming), [shop](https://shop.gavingogaming.com), [linkedin](https://www.linkedin.com/in/gavin-fox-149b40300/) }"
+        ];
+        break;
+      default:
+        newLines = [...newLines, `Command not found: ${args[0]}`];
+        break;
+    }
+  
+    // Update the state once with the new lines
+    setLines(newLines);
+  }
+  useEffect(()=>{
+    window.evalCommand = runCommand;
+  }, []);
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <div className="terminal">
+        <div className="terminal-lines">
+          {lines.map((line, index) => (
+            <div key={index} className="terminal-line">
+              <span dangerouslySetInnerHTML={{ __html: line }} />
+            </div>
+          ))}
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <div className="terminal-input">
+          <span dangerouslySetInnerHTML={{__html:`<span style="color:#0f0;">gavingogaming</span><span style="color:#f00;">@</span><span style="color:#0f0;">personal:</span><span style="color:#00f;">~</span><span style="color:#ff0;">$</span>`}}></span>
+          <input
+            type="text"
+            id="input"
+            autoCorrect={"off"}
+            autoCapitalize={"off"}
+            autoComplete={"off"}
+            onKeyDown={async (event) => {
+              const input = document.getElementById("input") as HTMLInputElement;
+              if (event.key === "Enter") {
+                runCommand(input.value);
+                input.value = "";
+              }
+            }}
+          />
+        </div>
       </div>
     </main>
   );
